51583b89a088280fb6cf2091e8078cd21f060548
Evgeniy <longlimp@gmail.com>
2022-09-12 13:18:09 +0200

    add fixed fix TestBase

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -3,7 +3,6 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.junit5.BrowserPerTestStrategyExtension;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.demoqa.data.TestData;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -12,45 +11,29 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
-public class TestBase {
 
-    public static final String
-            remote = System.getProperty("REMOTE"),
-            browserName = System.getProperty("browser_name", "chrome"),
-            browserVersion = System.getProperty("browser_version"),
-            browserSize = System.getProperty("browser_size", "800x800"),
-            LOGIN_REMOTE = "user1",
-            PASSWORD_REMOTE = "1234",
-            baseUrl = "https://demoqa.com",
-            selenoidURL = "selenoid.autotests.cloud/wd/hub";
+    @ExtendWith({BrowserPerTestStrategyExtension.class})
+    public class TestBase {
 
-    @BeforeAll
-    static void configure() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        @BeforeAll
+        static void configure() {
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            if (System.getProperty("selenide.remote") != null) {
+                Configuration.remote = System.getProperty("selenide.remote");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", true);
+            }
 
-        Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = baseUrl;
-        Configuration.browser = browserName;
-        Configuration.browserVersion = browserVersion;
-        Configuration.browserSize = browserSize;
+            Configuration.browserCapabilities = capabilities;
+            Configuration.baseUrl = "https://demoqa.com";
 
-        if (remote == null || remote.equals("")) {
-        } else {
-            Configuration.remote = "https://"
-                    + LOGIN_REMOTE + ":"
-                    + PASSWORD_REMOTE + "@"
-                    + selenoidURL;
-
+            Configuration.browser = System.getProperty("browser_name", "chrome");
+            Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+            Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
         }
 
-        if (browserVersion != null) {
-            Configuration.browserVersion = browserVersion;
-        }
-    }
 
     @AfterEach
     void addAttachments() {
@@ -58,10 +41,6 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-        if (remote == null || remote.equals("")) {
-        } else {
-            Attach.addVideo();
 
-        }
     }
 }
